6d72f1d547332aaadacadcbae872db72f7d25340
Tibe Calderon <tibecvp@gmail.com>
2025-05-25 15:42:41 -0400

    Updates the database users table every time the user profile image is updated in Clerk, the user session is started or ended.

--- convex/users.ts
+++ convex/users.ts
@@ -1,4 +1,4 @@
-import { v } from "convex/values"
+import { ConvexError, v } from "convex/values"
 import { internalMutation } from "./_generated/server"
 
 
@@ -17,5 +17,55 @@ export const createUser = internalMutation({
             image: args.image,
             isOnline: true
         })
+    }
+})
+
+export const updateUser = internalMutation({
+    args: {tokenIdentifier: v.string(), image: v.string()},
+    handler: async (ctx, args) => {
+        const user = await ctx.db
+            .query("users")
+            .withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", args.tokenIdentifier))
+            .unique()
+
+        if (!user) {
+            throw new ConvexError("No user with this token found")
+        }
+
+        await ctx.db.patch(user._id, {
+            image: args.image,
+        })
+    }
+})
+
+export const setUserOnline = internalMutation({
+    args: {tokenIdentifier: v.string()},
+    handler: async (ctx, args) => {
+        const user = await ctx.db
+        .query("users")
+        .withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", args.tokenIdentifier))
+        .unique()
+
+        if(!user) {
+            throw new ConvexError("User not found")
+        }
+
+        await ctx.db.patch(user._id, { isOnline: true })
+    }
+})
+
+export const setUserOffline = internalMutation({
+    args: {tokenIdentifier: v.string()},
+    handler: async (ctx, args) => {
+        const user = await ctx.db
+        .query("users")
+        .withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", args.tokenIdentifier))
+        .unique()
+
+        if(!user) {
+            throw new ConvexError("User not found")
+        }
+
+        await ctx.db.patch(user._id, { isOnline: false })
     }
 })
